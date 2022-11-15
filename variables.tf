f5db0567c0bf9f5919c45ab6b18b40a44ab43329
ngdtrunguit <ngdtrunguit@gmail.com>
2022-11-15 22:38:42 +0700

    variable

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "client_secret" {
+  type = string
+  sensitive = true
+}
+
+variable "client_id" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
+
+variable "subscription_id" {
+  type = string
+}
