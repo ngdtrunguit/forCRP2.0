05485b92298124315ec6b37d0a38abe87ccc8b2d
ngdtrunguit <ngdtrunguit@gmail.com>
2022-11-17 10:16:05 +0700

    aks

--- TF-aks/aks.tf
+++ TF-aks/aks.tf
@@ -18,23 +18,41 @@ resource "azurerm_kubernetes_cluster" "aks" {
     os_disk_size_gb = 50
   }
 
-  # service_principal {
-  #   client_id     = var.client_id
-  #   client_secret = var.client_secret
-  # }
   identity {
     type = "SystemAssigned"
   }
+  network_profile {
+    network_plugin = "kubenet"
+    network_policy = "calico"
+  }
+
   role_based_access_control_enabled = true
 
   tags = var.tags
 }
 
+
 resource "azurerm_container_registry" "acr" {
   name                = "trungnguyenprojectcrp2"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
+  admin_enabled       = true
 
   sku  = "Basic"
   tags = var.tags
+}
+
+data "azurerm_client_config" "current" {
+}
+
+data "azurerm_user_assigned_identity" "identity" {
+  name                = "${azurerm_kubernetes_cluster.aks.name}-agentpool"
+  resource_group_name = azurerm_kubernetes_cluster.aks.node_resource_group
+}
+
+resource "azurerm_role_assignment" "role_acrpull" {
+  scope                            = azurerm_container_registry.acr.id
+  role_definition_name             = "AcrPull"
+  principal_id                     = data.azurerm_user_assigned_identity.identity.principal_id
+  skip_service_principal_aad_check = true
 }
