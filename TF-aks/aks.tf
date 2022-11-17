78aa0cdc89173083420ebfd0ad0c579800ffac17
ngdtrunguit <ngdtrunguit@gmail.com>
2022-11-17 12:02:41 +0700

    ci

--- TF-aks/aks.tf
+++ TF-aks/aks.tf
@@ -21,10 +21,10 @@ resource "azurerm_kubernetes_cluster" "aks" {
   identity {
     type = "SystemAssigned"
   }
-  network_profile {
-    network_plugin = "kubenet"
-    network_policy = "calico"
-  }
+  # network_profile {
+  #   network_plugin = "kubenet"
+  #   network_policy = "calico"
+  # }
 
   role_based_access_control_enabled = true
 
@@ -37,22 +37,23 @@ resource "azurerm_container_registry" "acr" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   admin_enabled       = true
+  anonymous_pull_enabled = true
 
-  sku  = "Basic"
+  sku  = "Standard"
   tags = var.tags
 }
 
-data "azurerm_client_config" "current" {
-}
+# data "azurerm_client_config" "current" {
+# }
 
-data "azurerm_user_assigned_identity" "identity" {
-  name                = "${azurerm_kubernetes_cluster.aks.name}-agentpool"
-  resource_group_name = azurerm_kubernetes_cluster.aks.node_resource_group
-}
+# data "azurerm_user_assigned_identity" "identity" {
+#   name                = "${azurerm_kubernetes_cluster.aks.name}-agentpool"
+#   resource_group_name = azurerm_kubernetes_cluster.aks.node_resource_group
+# }
 
-resource "azurerm_role_assignment" "role_acrpull" {
-  scope                            = azurerm_container_registry.acr.id
-  role_definition_name             = "AcrPull"
-  principal_id                     = data.azurerm_user_assigned_identity.identity.principal_id
-  skip_service_principal_aad_check = true
-}
+# resource "azurerm_role_assignment" "role_acrpull" {
+#   scope                            = azurerm_container_registry.acr.id
+#   role_definition_name             = "AcrPull"
+#   principal_id                     = data.azurerm_user_assigned_identity.identity.principal_id
+#   skip_service_principal_aad_check = true
+# }
