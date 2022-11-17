29524c410e791748664d188eb5d081c61e634b00
ngdtrunguit <ngdtrunguit@gmail.com>
2022-11-17 16:23:41 +0700

    public ip

--- TF-aks/aks.tf
+++ TF-aks/aks.tf
@@ -5,18 +5,19 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                  = var.cluster_name
-  location              = azurerm_resource_group.rg.location
-  resource_group_name   = azurerm_resource_group.rg.name
-  dns_prefix            = var.dns_prefix
-  kubernetes_version    = var.kube_version
-  enable_node_public_ip = true
+  name                = var.cluster_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = var.dns_prefix
+  kubernetes_version  = var.kube_version
 
   default_node_pool {
-    name            = "default"
-    node_count      = 2
-    vm_size         = "Standard_D2_v2"
-    os_disk_size_gb = 50
+    name                  = "default"
+    node_count            = 2
+    vm_size               = "Standard_D2_v2"
+    os_disk_size_gb       = 50
+    enable_node_public_ip = true
+
   }
 
   identity {
