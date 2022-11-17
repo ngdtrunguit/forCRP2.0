58e483fcea74bb32d139c519c71663a49145a337
ngdtrunguit <ngdtrunguit@gmail.com>
2022-11-17 10:47:43 +0700

    helm

--- TF-helm/argo-rollouts.sh
+++ TF-helm/argo-rollouts.sh
@@ -12,5 +12,5 @@ hostname=$(kubectl get service istio-ingressgateway -n istio-ingress --output=js
 argocd login --username admin --password ${argocd_pwd} --grpc-web --insecure --plaintext ${hostname}:80
 
 #install blue-green app
-argocd app create --name blue-green --repo https://github.com/argoproj/argocd-example-apps --dest-server https://kubernetes.default.svc --dest-namespace application --path blue-green && argocd app sync blue-green
-# argocd app create --name blue-green --repo https://github.com/ngdtrunguit/Tim-lab.git --dest-server https://kubernetes.default.svc --dest-namespace application --path blue-green && argocd app sync blue-green
+# argocd app create --name blue-green --repo https://github.com/argoproj/argocd-example-apps --dest-server https://kubernetes.default.svc --dest-namespace application --path blue-green && argocd app sync blue-green
+argocd app create --name blue-green --repo https://github.com/ngdtrunguit/Tim-lab.git --dest-server https://kubernetes.default.svc --dest-namespace application --path blue-green && argocd app sync blue-green
