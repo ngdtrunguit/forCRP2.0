8db42d44d63ad7490a325f053b5d60f93f56ae2b
ngdtrunguit <ngdtrunguit@gmail.com>
2022-11-18 09:58:21 +0700

    sh

--- TF-helm/argo-rollouts.sh
+++ TF-helm/argo-rollouts.sh
@@ -3,6 +3,11 @@
 #install Argo Rollout
 kubectl apply -n ${namespace} -f https://github.com/argoproj/argo-rollouts/releases/latest/download/install.yaml
 
+while
+[ $(kubectl -n argocd get pod -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]; do
+ sleep 1
+done
+
 #Get credential to argocd
 argocd_pwd=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
 hostname=$(kubectl get service istio-ingressgateway -n istio-ingress --output=jsonpath='{.status.loadBalancer.ingress[0].ip}')
