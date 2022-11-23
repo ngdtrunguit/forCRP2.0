97afd5f555342680ff17737365df8e5ed5cc9ff0
ngdtrunguit <ngdtrunguit@gmail.com>
2022-11-23 09:47:45 +0700

    jenkins

---
+++ TF-helm/jenkins.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#install jenkins
+helm repo add jenkins https://charts.jenkins.io
+helm repo update
+helm -n jenkins upgrade --install jenkins jenkins/jenkins --values values.yaml 
+
+helm -n ${namespace} upgrade --install  -f jenkins/jenkins --values values-jenkins.yaml
+
+
+kubectl create secret docker-registry docker-credentials \
+ --docker-server=trungnguyenprojectcrp2.azurecr.io \
+ --docker-username=trungnguyenprojectcrp2 \
+ --docker-password=${acr_pwd} \
+ --docker-email=ngdtrunguit@gmail.com \
+ --namespace jenkins
+
+ kubectl apply -n ${namespace} -f ./jenkins-virtualservice.yaml
