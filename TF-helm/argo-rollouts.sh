30fb28ac2c2f769f0d9ac78bb70de45692842f09
ngdtrunguit <ngdtrunguit@gmail.com>
2022-11-24 12:00:59 +0700

    a

--- TF-helm/argo-rollouts.sh
+++ TF-helm/argo-rollouts.sh
@@ -3,9 +3,9 @@
 #install Argo Rollout
 kubectl apply -n ${namespace} -f https://github.com/argoproj/argo-rollouts/releases/latest/download/install.yaml
 
-#wait until argocd server ready
+#wait until argocd server & argo-rollouts are ready
 kubectl -n argocd wait pods -l app.kubernetes.io/name=argocd-server --for condition=Ready --timeout=60s
-
+kubectl -n argocd wait pods -l app.kubernetes.io/name=argo-rollouts --for condition=Ready --timeout=60s
 
 #Get credential to argocd
 argocd_pwd=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
