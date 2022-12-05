888adde1fffa8287159b892b417870a498c72b57
ngdtrunguit <ngdtrunguit@gmail.com>
2022-12-05 12:59:44 +0700

    remove timeout

--- TF-helm/argo-rollouts.sh
+++ TF-helm/argo-rollouts.sh
@@ -4,8 +4,8 @@
 kubectl apply -n ${namespace} -f https://github.com/argoproj/argo-rollouts/releases/latest/download/install.yaml
 
 #wait until argocd server & argo-rollouts are ready
-kubectl -n argocd wait pods -l app.kubernetes.io/name=argocd-server --for condition=Ready --timeout=60s
-kubectl -n argocd wait pods -l app.kubernetes.io/name=argo-rollouts --for condition=Ready --timeout=60s
+kubectl -n argocd wait pods -l app.kubernetes.io/name=argocd-server --for condition=Ready
+kubectl -n argocd wait pods -l app.kubernetes.io/name=argo-rollouts --for condition=Ready
 
 #Get credential to argocd
 argocd_pwd=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
